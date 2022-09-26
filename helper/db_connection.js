8594142669fb74eb2b1b3a2b6c23d659d2c3c955
Gyga Maulana <gygamaulana@gmail.com>
2022-09-26 13:18:23 +0700

    add : db_connection

--- helper/db_connection.js
+++ helper/db_connection.js
@@ -1,5 +1,5 @@
 //connect ke database
-const mysql = require("mysql2");
+const mysql = require("mysql");
 const {host, user, password, database} = process.env
 const db = mysql.createConnection({
   host      : host,
